c09280a5c753d4543dfa4fb4ced86500f85053f6
Roscalion <Roscalion@users.noreply.github.com>
2022-03-16 14:22:02 -0600

    Add files via upload
    Here is my SGBA stats for my point guard. He has won two championships already in the NCAA and the FIAA championships. As he goes through the pro seasons, this will be the stats Javascript code to remember.

---
+++ Rosca Santigria - SGBA Stat Prorgession.js
@@ -0,0 +1,60 @@
+
+// Season 2 - Episode 16 - Rosca and SGBA Stat Progressions 
+// Create a Code for all of my SGBA Basketball Stats. 
+
+const basketballStats = {
+// Finishing Stats 
+    layup: 82,
+    standDunk: 33,
+    dunk: 80,
+    close: 85,
+
+// Shooting Stats 
+    midShot: 8,
+    threeShot: 90,
+    freeThrow: 80,
+    postHook: 36,
+    postFade: 51,
+    PostC: 44,
+    foul: 65,
+    shotIQ: 82,
+    
+// Playmaking Stats
+    pass: 91,
+    ball: 85,
+    speedWithBall: 86,
+    hands: 88,
+    passIQ: 85,
+    passVision: 85,
+    offensiveConsitency: 77,
+
+
+// Defense Stats
+    
+    interiorDefense: 41,
+    perimeterDefense: 77,
+    steal: 76,
+    block: 39,
+    offensiveRebound: 36,
+    defensiveRebound: 50,
+    latteralQuicknes: 85,
+    helpDefenseIQ: 78,
+    passPerception: 85,
+    defensiveConsistency: 74,
+ 
+ // Physical Stats 
+ 
+    speed: 86,
+    acceleration: 89,
+    strength: 68,
+    veritcal: 75,
+    stamina: 99,
+    intangibles: 70,
+    hustle: 76,
+    durability: 79,
+
+ 
+
+}
+
+console.log(basketballStats)
